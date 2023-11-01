8700d29e979b3e29fc7867b582cb62e0eb71cf1c
Arctic-Furnace <richardjameshaddon@gmail.com>
2023-11-01 14:10:33 +0000

    02 repeats a string and passes all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if( num >= 0 )
+    {
+        let multipleString = '';
+        for(let i=0; i<num;i++)
+        {
+            multipleString += string;
+        }
+        return multipleString;
+    }
+    else
+    {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
