e40b06348cc66c208a04ddeb607912e9b6151174
Arctic-Furnace <richardjameshaddon@gmail.com>
2023-11-02 14:55:33 +0000

    04 removes items from array and passes all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
 const removeFromArray = function() {
-
+    //let arrayItems = arguments[0];
+    for(let j = 1; j < arguments.length + 1; j++)
+    {
+        for (let i = 0; i < arguments[0].length; i++ )
+        {
+            if(arguments[0][i] === arguments[j])
+            {
+                arguments[0].splice(i,1);
+            }
+        }
+    }
+    return arguments[0];
 };
 
 // Do not edit below this line
