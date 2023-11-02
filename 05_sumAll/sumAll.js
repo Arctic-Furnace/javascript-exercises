9a79cd29181d40a8ba33459f6006fbb281a95360
Arctic-Furnace <richardjameshaddon@gmail.com>
2023-11-02 15:16:28 +0000

    05 sums all numbers and passes all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,36 @@
 const sumAll = function() {
 
+    if (
+        typeof arguments[0] != "number" 
+        || typeof arguments[1] != "number" 
+        || arguments[0] < 0 
+        || arguments[1] < 0)
+    {
+        return "ERROR";
+    }
+
+
+    let total = 0;
+    let smallNumber = 0;
+    let largeNumber = 0;
+
+    if(arguments[0] < arguments[1])
+    {
+        smallNumber = arguments[0];
+        largeNumber = arguments[1];
+    }
+    else
+    {
+        smallNumber = arguments[1];
+        largeNumber = arguments[0];
+    }
+
+    for (let i = smallNumber; i <= largeNumber; i++)
+    {
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
