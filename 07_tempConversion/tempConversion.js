44a70b649067823c2154885a067d52e3b7fad487
Arctic-Furnace <richardjameshaddon@gmail.com>
2023-11-03 15:52:01 +0000

    07 Temperature Conversion and passes all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  temperature = (temperature - 32) * (5/9);
+  let result = temperature.toFixed(1);
+  return parseFloat(result);
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  temperature = (temperature * (9/5)) + 32;
+  let result = temperature.toFixed(1);
+  return parseFloat(result);
 };
 
 // Do not edit below this line
