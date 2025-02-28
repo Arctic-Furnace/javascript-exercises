70dd7b1c5b39a8530d1a8099a09fe7895e09a9f5
Arctic-Furnace <richardjameshaddon@gmail.com>
2025-02-28 11:25:59 +0000

    Add calculator task

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((current,next) => current + next,0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((current,next) => current * next, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let total = 1;
+  for(let i = b; i > 0; i--){
+    total *= a;
+  };
+  return total;
+  //return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+	for(let i = a; i > 0; i--){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
