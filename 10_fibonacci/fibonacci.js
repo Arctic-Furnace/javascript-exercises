22c75864e939a0c708491a4c7050f4b22c5a7e8a
Arctic-Furnace <richardjameshaddon@gmail.com>
2025-02-28 14:12:46 +0000

    Add fibonacci task

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    if(a < 0){
+        return "OOPS";
+    }
 
+    let firstNum = 0;
+    let secondNum = 1;
+
+    for(let i = 1; i < a; i++){
+        let next = firstNum + secondNum;
+        firstNum = secondNum;
+        secondNum = next;
+    }
+    return secondNum;
 };
 
 // Do not edit below this line
