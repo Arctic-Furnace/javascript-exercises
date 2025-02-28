03944e05d92d6d14b083a12db27f318c632080c7
Arctic-Furnace <richardjameshaddon@gmail.com>
2025-02-28 11:41:54 +0000

    Add palindromes task

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    //replace removes
+    const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    //split will create an array of the letters in the string, you can't just reverse a string without creating a new function
+    return processedString === processedString.split("").reverse().join("");
 };
 
 // Do not edit below this line
