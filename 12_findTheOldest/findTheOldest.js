580bea0f23227ceb04c82fc2dc718c65323f978c
Arctic-Furnace <richardjameshaddon@gmail.com>
2025-02-28 14:46:43 +0000

    Add Find the oldest task

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce(function(oldest, currentPerson) {
+        const lastPerson = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const nextPerson = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return (nextPerson < lastPerson) ? oldest : currentPerson;
+    } );
 };
+
+const getAge = function(birthYear, deathYear){
+    if(!deathYear){
+        deathYear = new Date().getFullYear();
+    }
+    return deathYear - birthYear;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
